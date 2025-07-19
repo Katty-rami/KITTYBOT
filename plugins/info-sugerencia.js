6c2f8f5e42d913ae3ce0ab21a4f23d28538c87ed
KillzN <scorpionsheep203@gmail.com>
2025-07-19 00:25:55 -0400

    OCEAN BOT

--- plugins/info-sugerencia.js
+++ plugins/info-sugerencia.js
@@ -1,7 +1,7 @@
 //Adaptado & Mejorado por Criss Escobar 
 
 let handler = async (m, { conn, text }) => {
-    const suittag = '56983073328';
+    const suittag = '529862177201';
     const user = m.pushName || 'Usuario';
 
     if (!text) {
